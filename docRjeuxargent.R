33a4c3c5c56edb08d906b3548f4e93b069828af4
AGasowski <alexandre_gasowski@yahoo.com>
2025-02-12 19:44:54 +0100

    poires

--- docRjeuxargent.R
+++ docRjeuxargent.R
@@ -1,11 +1,4 @@
-#Premier test de modification
-#Deuxieme modification
-#Troisieme modification
-#4e modification
-#6e modification
-#7
-<<<<<<< HEAD
-#9
-=======
-#8
->>>>>>> 3196f4b340f7641c18e1ac034cdc482ec3c33f39
+#Explication d'un conflit
+
+
+#J'aime les poires
