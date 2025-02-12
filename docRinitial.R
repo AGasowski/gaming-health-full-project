01c0e70a1fbddbf0e8e2553d36024154b3153106
AGasowski <alexandre_gasowski@yahoo.com>
2025-02-12 20:20:20 +0100

    Nouvelle base de données

--- docRinitial.R
+++ docRinitial.R
@@ -1,8 +1,11 @@
+rm(list=ls())
+
 install.packages("dplyr")
 library(dplyr)
 
-data <- read.csv2("Enquête 2022-20250104/bdd_2022.csv", header = TRUE)
+datainitial <- read.csv2("Enquête 2022-20250104/bdd_2022.csv", header = TRUE)
 
-#datautiles contient tous les individus qui ont répondu au questionnaire sur les jeux
-datautiles <- data %>% 
+#data contient tous les individus qui ont répondu
+#au questionnaire sur les jeux
+data <- datainitial %>% 
   filter(!is.na(QB01A) & !is.na(QB01B)  & !is.na(QB01C)  & !is.na(QB01D))
