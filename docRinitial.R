413941ea8bf35b57a3f743fdccc9aab6fe0fca3a
noah4217 <noah.marli1@gmail.com>
2025-03-01 20:02:30 +0100

    update

--- docRinitial.R
+++ docRinitial.R
@@ -1,6 +1,9 @@
 rm(list=ls())
 
 install.packages("dplyr")
+install.packages("vcd")
+
+library(vcd)
 library(dplyr)
 
 datainitial <- read.csv2("Enquête 2022-20250104/bdd_2022.csv", header = TRUE)
@@ -10,9 +13,7 @@ datainitial <- read.csv2("Enquête 2022-20250104/bdd_2022.csv", header = TRUE)
 data <- datainitial %>% 
   filter(!is.na(QB01A) & !is.na(QB01B)  & !is.na(QB01C)  & !is.na(QB01D))
 
-
 # catégories IMC
-
 data <- data %>% 
   mutate(santephysique = case_when(
     q18imc < 17 | q18imc > 30 ~ "très mauvaise",
