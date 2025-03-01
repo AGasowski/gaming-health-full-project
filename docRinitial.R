6c7419d8d4847ffeb59ce330dabbf6b9513ab264
AGasowski <alexandre_gasowski@yahoo.com>
2025-03-01 19:31:29 +0100

    IMC

--- docRinitial.R
+++ docRinitial.R
@@ -9,3 +9,13 @@ datainitial <- read.csv2("Enquête 2022-20250104/bdd_2022.csv", header = TRUE)
 # au questionnaire sur les jeux
 data <- datainitial %>% 
   filter(!is.na(QB01A) & !is.na(QB01B)  & !is.na(QB01C)  & !is.na(QB01D))
+
+
+# catégories IMC
+
+data <- data %>% 
+  mutate(santephysique = case_when(
+    q18imc < 17 | q18imc > 30 ~ "très mauvaise",
+    q18imc < 18.5 | q18imc > 25 ~ "plutôt mauvaise",
+    TRUE ~ "bonne"
+  ))
