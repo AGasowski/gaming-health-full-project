fdb70606461f8a23adea4e545f46a97c83864608
noah4217 <noah.marli1@gmail.com>
2025-03-01 20:11:23 +0100

    update

--- docRinitial.R
+++ docRinitial.R
@@ -41,6 +41,8 @@ data$score_ADRS_categorie <- cut(data$score_ADRS,
                                  breaks = c(-1, 3, 6, 11),
                                  labels = c("Peu de risque d’EDC", "Risque d’EDC modéré", "Risque d’EDC important"))
 
+# EDC = Épisode dépressif caractérisé
+
 #Création data_réduit qui contient uniquement les variables crées et modifiées
 data_reduit <- data[, c("score_ADRS", "score_ADRS_categorie", "IMC", "IMC_categorie")]
 
